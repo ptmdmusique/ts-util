137b8c1f5074f084c2ee9be7addd0608d2a13e2e
ptmdmusique <ptmdmusic@gmail.com>
2021-12-04 23:00:13 -0800

    fix package json and tweak ombb

--- math/angle-between-line-and-x.ts
+++ math/angle-between-line-and-x.ts
@@ -1,4 +1,8 @@
-const x = 0;
-const y = 1;
+export const getVectorAngleToXAxis = (x: number, y: number) =>
+  (Math.atan2(y, x) * 180) / Math.PI;
 
-console.log(`Angle: ${(Math.atan2(y, x) * 180) / Math.PI}`);
+const test = () => {
+  const x = 0;
+  const y = 1;
+  console.log(`Angle: ${getVectorAngleToXAxis(x, y)}`);
+};
