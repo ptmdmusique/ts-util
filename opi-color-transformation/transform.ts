0fcdd87b1c729e65d77abef7ec0b0e5045da119b
Duc Phan <ptmdmusic@gmail.com>
2022-01-04 22:24:42 -0800

    update OPI transform script to handle patterns

--- opi-color-transformation/transform.ts
+++ opi-color-transformation/transform.ts
@@ -6,12 +6,64 @@ interface BaseColor {
   hexValue: string;
 }
 
-interface NailColor {
+interface BaseNail {
   name: string;
+}
+interface NailColor extends BaseNail {
   colorGroup: string;
   /** RGB */
   color: [number, number, number];
-  sku: string;
+  sku: string | null;
+}
+
+interface NailPattern extends BaseNail {
+  imageUrl: string;
+}
+
+const colorGroupList = [
+  "Black",
+  "Brown",
+  "Blue",
+  "Purple",
+  "Red",
+  "Green",
+  "Nude",
+  "Metallic",
+  "Yellow",
+  "Pink",
+  "Orange",
+  "White",
+] as const;
+
+type CollectionName = typeof colorGroupList[number] | "Patterns";
+
+// const colorGroupImageMap: Record<ColorGroup, string> = {
+//   Black:
+//     "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Maps_Pin_FullColor.max-1000x1000.png",
+//   Blue: "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Maps_Pin_FullColor.max-1000x1000.png",
+//   Brown:
+//     "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Maps_Pin_FullColor.max-1000x1000.png",
+//   Green:
+//     "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Maps_Pin_FullColor.max-1000x1000.png",
+//   Metallic:
+//     "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Maps_Pin_FullColor.max-1000x1000.png",
+//   Nude: "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Maps_Pin_FullColor.max-1000x1000.png",
+//   Orange:
+//     "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Maps_Pin_FullColor.max-1000x1000.png",
+//   Pink: "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Maps_Pin_FullColor.max-1000x1000.png",
+//   Purple:
+//     "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Maps_Pin_FullColor.max-1000x1000.png",
+//   Red: "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Maps_Pin_FullColor.max-1000x1000.png",
+//   White:
+//     "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Maps_Pin_FullColor.max-1000x1000.png",
+//   Yellow:
+//     "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Maps_Pin_FullColor.max-1000x1000.png",
+// };
+
+interface ColorCollection {
+  name: CollectionName;
+  thumbnailUrl: string;
+  colors: (NailColor | NailPattern)[];
 }
 
 function hexToRgb(hex: string): [number, number, number] | null {
@@ -36,26 +88,34 @@ const getColorGroup = (baseColorGroup: string) => {
   }
 };
 
+const getThumbnailUrl = (groupName: string) =>
+  `https://storage.googleapis.com/color-collection-thumbnail/${groupName}.jpg`;
+
+const getPatternUrl = (patternName: string) =>
+  `https://storage.googleapis.com/pattern-collection-bucket/${patternName}.jpg`;
+
 const getColorDataList = (baseColorList: BaseColor[]): NailColor[] =>
   baseColorList.map((info) => {
     const colorGroup = info.colorGroup;
     if (!colorGroup) {
       console.log("Missing color group", info);
     } else if (colorGroup.split("/").length > 1) {
-      console.log("Odd color group", colorGroup);
+      // console.log("Odd color group", colorGroup);
     }
 
     return {
       name: info.name,
-      sku: "unknown", // TODO: Change this
+      sku: null, // TODO: Change this
       color: hexToRgb(info.hexValue) || [0, 0, 0],
       colorGroup: getColorGroup(colorGroup || "Nude"),
+      image: null,
     };
   });
 
 const start = async () => {
   const colorMap: Record<string, BaseColor> = {};
 
+  // * --- Color patterns
   readdirSync("./").forEach((file) => {
     const extension = file.split(".").pop();
     if (extension === "json") {
@@ -71,9 +131,55 @@ const start = async () => {
 
   const transformedData = getColorDataList(Object.values(colorMap));
 
+  const outputData: Record<string, ColorCollection> = {};
+  transformedData.forEach((color) => {
+    const colorGroup = color.colorGroup;
+
+    if (!outputData[colorGroup]) {
+      outputData[colorGroup] = {
+        name: colorGroup as CollectionName,
+        thumbnailUrl: getThumbnailUrl(colorGroup.toLocaleLowerCase()),
+        colors: [color],
+      };
+    } else {
+      outputData[colorGroup].colors.push(color);
+    }
+  });
+
+  const finalData = Object.values(outputData)
+    .sort((a, b) => (a.name === "Red" ? -1 : a.name.localeCompare(b.name)))
+    .map((colorCollection) => {
+      return {
+        ...colorCollection,
+        colors: colorCollection.colors.sort((a, b) =>
+          a.name === "Big Apple Red" ? -1 : a.name.localeCompare(b.name),
+        ),
+      };
+    });
+
+  // * --- Patterns
+  const patternsList: NailPattern[] = readdirSync("./nail-patterns").map(
+    (fileName) => {
+      const patternName = fileName.split(".").shift() || "";
+      const capitalizedName = patternName
+        .toLowerCase()
+        .split(" ")
+        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(" ");
+
+      return { name: capitalizedName, imageUrl: getPatternUrl(patternName) };
+    },
+  );
+
+  finalData.push({
+    name: "Patterns",
+    thumbnailUrl: getThumbnailUrl("pattern"),
+    colors: patternsList,
+  });
+
   writeFileSync(
     "./output.js",
-    `export const nailColorData = ${JSON.stringify(transformedData)};`,
+    `export const nailColorData = ${JSON.stringify(finalData)};`,
   );
 };
 
